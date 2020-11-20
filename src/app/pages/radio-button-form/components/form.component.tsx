d5716127e249cbed294587caf7ab0b333ccf4260
warjohn123 <warrencaruana1@gmail.com>
2020-11-20 18:26:05 +0800

    Add form

--- src/app/pages/radio-button-form/components/form.component.tsx
+++ src/app/pages/radio-button-form/components/form.component.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { apiResult } from "./api-result";
+import styles from "../form.module.scss";
+import { Col, Row } from "antd";
 
 function FormComponent() {
-  return <>Hello world!</>;
+  const submit = () => {
+    console.log("test");
+  };
+  console.log("api result", apiResult);
+  return (
+    <>
+      <form onSubmit={submit}>
+        {apiResult.menus.map((group, index) => (
+          <div key={index} className={styles.GroupContainer}>
+            {group.map((menu) => {
+              return (
+                <div key={menu.id} className={styles.RadioContainer}>
+                  <input
+                    type="radio"
+                    name={index.toString()}
+                    id={menu.id}
+                    value={menu.value}
+                  ></input>
+                  <label style={{ marginLeft: 8 }}>{menu.value}</label>
+                </div>
+              );
+            })}
+          </div>
+        ))}
+
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 }
 
 export default FormComponent;
